21f08c432598de8a9bfb7b7f8fe39ad1c8e3d332
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-10 12:05:29 -0600

    Refactor server configuration and add PORT constant
    - Moved dotenv configuration to the top of src/index.js for better readability.
    - Introduced a new config file src/config/index.js to define the PORT constant.
    - Updated src/index.js to use the PORT constant from the new config file.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import { getProjects } from "./utils/projects.js";
+import { PORT } from "./config/index.js";
 
-dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 // 🔹 Necesario en ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+console.log({ PORT });
 
 app.use(express.json());
 
