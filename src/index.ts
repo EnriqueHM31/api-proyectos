aa2193d0d7bf7a8f30f69f31d9a148afdd76be41
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-10 13:20:20 -0600

    Add TypeScript support and restructure project files
    - Introduced TypeScript configuration with tsconfig.json for improved type checking and compilation.
    - Updated package.json to include TypeScript and related development dependencies.
    - Implemented server logic in src/index.ts, utilizing TypeScript for type safety.
    - Created utility functions in src/utils/projects.ts to manage project data.
    - Configured Express to render views using EJS, enhancing the project structure.

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,17 @@
 import dotenv from "dotenv";
-dotenv.config();
 import express from "express";
 import path from "path";
-import { fileURLToPath } from "url";
 import { getProjects } from "./utils/projects.js";
 import { PORT } from "./config/index.js";
-
+dotenv.config();
 
 const app = express();
-
-// 🔹 Necesario en ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-console.log({ PORT });
-
 app.use(express.json());
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
     res.render("home", {
         projects: getProjects(),
     });
