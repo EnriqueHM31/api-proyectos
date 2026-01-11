0253182c38fc4872c09a405485fcc7643f24ba8d
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-11 14:46:01 -0600

    refactor: update routing and static file serving in the Express app
    - Replaced the default router with homeRouter for improved organization.
    - Added geoIpRouter for geolocation-related routes.
    - Configured Express to serve static files from the public directory.
    - Enhanced project structure by clearly separating sections in the index file.

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,27 @@ import dotenv from "dotenv";
 import express from "express";
 import path from "path";
 import { PORT } from "./config/index.js";
-import router from "./routes/index.js";
+import { homeRouter } from "./routes/index.js";
 import { SRC_ROOT } from "./utils/index.js";
+import { geoIpRouter } from "./routes/geoIp.routes.js";
 
+/*------------------ ENV ------------------*/
 dotenv.config();
 
+/*------------------ APP ------------------*/
 const app = express();
 app.use(express.json());
 
+/*------------------ VIEWS ------------------*/
 app.set("view engine", "ejs");
 app.set("views", path.join(SRC_ROOT, "views"));
+app.use(express.static(path.join(SRC_ROOT, "public")));
 
-app.use(router);
+/*------------------ ROUTES ------------------*/
+app.use(homeRouter);
+app.use("/geolocalizacion", geoIpRouter);
 
+/*------------------ LISTEN ------------------*/
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
