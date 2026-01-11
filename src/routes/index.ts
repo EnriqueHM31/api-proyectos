8ba1177fea328df23de536ca51251c4f415f8c86
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-11 14:46:32 -0600

    refactor: update homeRouter and enhance home view
    - Renamed the default router to homeRouter for better clarity in routing.
    - Added a shortcut icon link in the home view for improved user experience.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,7 @@
 import { Router } from "express";
 import { viewRoutes } from "./view.routes.js";
 
-const router = Router();
+export const homeRouter = Router();
 
-router.use("/", viewRoutes);
+homeRouter.use("/", viewRoutes);
 
-export default router;
