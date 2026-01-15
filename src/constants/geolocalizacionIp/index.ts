6dfdad3591e10816fe32fc5a033a544721defeb9
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-14 22:59:01 -0600

    feat: consolidate geolocation constants into a single index.ts file
    - Moved geolocation IP constants from the previous file to a new index.ts file for better organization.
    - Added weather API constants to the same file, streamlining access to related configuration values.

--- src/constants/geolocalizacionIp/index.ts
+++
@@ -1,2 +0,0 @@
-export const URL_GEOLOCALIZACION_IP = "https://api.ipgeolocation.io/v2/ipgeo?";
-export const API_KEY_IP_GEOLOCALIZACION = process.env.API_KEY_IP ?? "";
---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+/*------------------ GEOLOCALIZACION IP ------------------*/
+export const URL_GEOLOCALIZACION_IP = "https://api.ipgeolocation.io/v2/ipgeo?";
+export const API_KEY_IP_GEOLOCALIZACION = process.env.API_KEY_IP ?? "";
+
+/*------------------ CLIMA ------------------*/
+export const BASE_URL_CLIMA = "http://api.weatherapi.com/v1/forecast.json?key=";
+export const API_KEY_CLIMA = process.env.API_KEY_CLIMA ?? "";
