fc368156d10c27058d35c01e43030402b6378c6f
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-14 22:59:16 -0600

    refactor: update geolocation constants import path
    - Changed the import path for geolocation IP constants to a consolidated index.ts file for improved organization and maintainability.

--- src/routes/geoIp.routes.ts
+++ src/routes/geoIp.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { IpController } from "../controllers/ip.controller.js";
-import { URL_GEOLOCALIZACION_IP, API_KEY_IP_GEOLOCALIZACION } from "../constants/geolocalizacionIp/index.js";
+import { URL_GEOLOCALIZACION_IP, API_KEY_IP_GEOLOCALIZACION } from "../constants/index.js";
 import { creacionAPI } from "../utils/Geolocalizacion/index.js";
 
 const url = creacionAPI(URL_GEOLOCALIZACION_IP, API_KEY_IP_GEOLOCALIZACION);
