48239369825b07a1802e67bf61416f0328d8594f
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-16 11:10:50 -0600

    refactor: eliminar opciones innecesarias del middleware CORS

--- src/middleware/cors.middleware.ts
+++ src/middleware/cors.middleware.ts
@@ -3,7 +3,4 @@ import cors from "cors";
 export const corsMiddleware = cors({
     origin: "http://localhost:5173",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
 });
