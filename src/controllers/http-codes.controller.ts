9e4aeebbc7a6780deca0d466713e63df4aae313f
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-16 21:55:31 -0600

    feat: renombrar método getHttpCodes a getHttpCodesAll y mejorar la lógica de obtención de códigos HTTP

--- src/controllers/http-codes.controller.ts
+++ src/controllers/http-codes.controller.ts
@@ -2,9 +2,22 @@ import HttpCodes from "../data/http-codes.json" with { type: "json" };
 import type { Request, Response } from "express";
 
 export class HttpCodesController {
-    getHttpCodes = (_req: Request, res: Response) => {
+    getHttpCodesAll = (_req: Request, res: Response) => {
         console.log("Solicitud recibida para obtener códigos HTTP");
 
         res.status(200).json(HttpCodes);
+    };
+
+    getHttpCode = (req: Request, res: Response) => {
+        console.log("Solicitud recibida para obtener un código HTTP");
+
+        const code = req.params.code;
+        const httpCode = HttpCodes.find((c) => Number(c.code) === Number(code));
+
+        if (!httpCode) {
+            res.status(404).json({ error: "No se encontró el código" });
+        } else {
+            res.status(200).json(httpCode);
+        }
     };
 }
