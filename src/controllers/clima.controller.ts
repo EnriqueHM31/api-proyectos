3ef12b513f627cbcc01b12062764e82a50b0e052
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-18 21:26:22 -0600

    refactor: simplify getClima method by using utility functions for validation and URL creation

--- src/controllers/clima.controller.ts
+++ src/controllers/clima.controller.ts
@@ -1,43 +1,33 @@
 import type { Request, Response } from "express";
+import { ClimaModel } from "../models/local/clima.model.js";
+import { crearUrlClima, validarDays, validarString } from "../utils/Clima/index.js";
+
+const climaModel = new ClimaModel();
+
 export class ClimaController {
     constructor(private readonly urlClima: string) { }
 
     getClima = async (req: Request, res: Response) => {
         try {
-            const { lugar } = req.params;
-            const { days } = req.query;
+            const { lugar: lugarParam } = req.params;
+            const { days: daysParam } = req.query;
 
             // 1️⃣ Validar existencia
-            if (!lugar) {
+            if (!lugarParam) {
                 res.status(400).json({
                     error: {
                         code: 1003,
                         message: "Lugar requerido"
                     }
                 });
-                throw new Error("Lugar requerido");
             }
 
+            const days = validarDays(daysParam);
+            const lugar = validarString(lugarParam);
 
-            const url = `${this.urlClima}&q=${encodeURIComponent(lugar as string)}&days=${encodeURIComponent(days as string)}`;
+            const url = crearUrlClima(this.urlClima, { lugar, days });
 
-            // 3️⃣ Llamada a API externa
-            const response = await fetch(url);
-
-            // 4️⃣ Manejo de error HTTP
-            if (!response.ok) {
-                const data = await response.json() as { error?: { code: number, message: string } };
-                res.status(response.status).json({
-                    error: {
-                        code: data.error?.code,
-                        message: data.error?.message
-                    }
-                });
-                throw new Error("Error API externa");
-            }
-
-            // 5️⃣ Parseo seguro
-            const data = await response.json();
+            const data = await climaModel.getClima({ url });
 
             res.status(200).json(data);
 
