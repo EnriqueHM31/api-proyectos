e3ab2893208a9c314c9b8669eba9e4e11abea5d0
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-18 21:43:37 -0600

    feat: create HttpCodesModel for retrieving and handling HTTP codes with error management

---
+++ src/models/local/http-codes.model.ts
@@ -0,0 +1,26 @@
+import HttpCodes from "../../data/http-codes.json" with { type: "json" };
+
+
+export class HttpCodesModel {
+    async getHttpCodes() {
+        if (!HttpCodes) {
+            throw new Error("No hay códigos HTTP disponibles");
+        }
+
+        return HttpCodes;
+    }
+
+    async getHttpCode({ code }: { code: number }) {
+        if (!HttpCodes) {
+            throw new Error("No hay códigos HTTP disponibles");
+        }
+
+        const httpCode = HttpCodes.find((httpCode) => httpCode.code === code);
+
+        if (!httpCode) {
+            throw new Error("Código HTTP no encontrado");
+        }
+
+        return httpCode;
+    }
+}
