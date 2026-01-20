eec070201d2a4296aa5aa11545ecae37bfaff9b2
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-19 21:19:03 -0600

    refactor: implement createCorsMiddleware function to enhance CORS handling with a list of allowed origins and improved error handling

--- src/middleware/cors.middleware.ts
+++ src/middleware/cors.middleware.ts
@@ -1,6 +1,33 @@
-import cors from "cors";
+// src/middlewares/cors.middleware.ts
+import cors, { type CorsOptions } from 'cors';
 
-export const corsMiddleware = cors({
-    origin: "http://localhost:5173",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-});
+
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://api-proyectos.vercel.app',
+    'http://localhost:5173',
+    'http://localhost:4321',
+    'http://192.168.1.73:5173/',
+    'http://192.168.1.73:4321/',
+    'http://192.168.1.73:5173',
+
+
+];
+export function createCorsMiddleware() {
+    const options: CorsOptions = {
+        origin(origin, callback) {
+            // Permite requests sin origin (Postman, curl, SSR)
+            if (!origin) return callback(null, true);
+
+            if (allowedOrigins.includes(origin)) {
+                return callback(null, true);
+            }
+
+            return callback(new Error(`CORS not allowed for origin: ${origin}`));
+        },
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        credentials: true,
+    };
+
+    return cors(options);
+}
