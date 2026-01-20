23099f570913bda4b37cb4d58db4b5a59cedd331
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-19 22:56:22 -0600

    feat: add API_KEY_EMAIL constant for email service integration

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,3 +5,5 @@ export const API_KEY_IP_GEOLOCALIZACION = process.env.API_KEY_IP ?? "";
 /*------------------ CLIMA ------------------*/
 export const BASE_URL_CLIMA = "http://api.weatherapi.com/v1/forecast.json?key=";
 export const API_KEY_CLIMA = process.env.API_KEY_CLIMA ?? "";
+
+export const API_KEY_EMAIL = process.env.API_KEY_EMAIL ?? "";
