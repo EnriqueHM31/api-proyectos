4097bab1acfe8c22d0d522b579228e08617da0f3
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-19 21:18:55 -0600

    refactor: replace corsMiddleware with createCorsMiddleware for improved CORS handling and update server listening address to a specific IP

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 import { PORT } from "./config/index.js";
 import { setupStatic } from "./config/static.js";
 import { setupViews } from "./config/view.js";
-import { corsMiddleware } from "./middleware/cors.middleware.js";
+import { createCorsMiddleware } from "./middleware/cors.middleware.js";
 import { climaRouter } from "./routes/clima.routes.js";
 import { geoIpRouter } from "./routes/geoIp.routes.js";
 import { homeRouter } from "./routes/home.routes.js";
@@ -11,7 +11,7 @@ import { httpCodesRouter } from "./routes/http-codes.routes.js";
 
 /*------------------ APP ------------------*/
 const app = express();
-app.use(corsMiddleware);
+app.use(createCorsMiddleware());
 app.use(express.json());
 
 /*------------------ VIEWS ------------------*/
@@ -25,6 +25,6 @@ app.use("/clima", climaRouter);
 app.use("/http-codes", httpCodesRouter);
 
 /*------------------ LISTEN ------------------*/
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '192.168.1.73', () => {
+    console.log(`Server running on http://192.168.1.73:${PORT}`);
 });
