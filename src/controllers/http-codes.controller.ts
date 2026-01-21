b3a5a29e12b43c817c7c5a68bedb20facd5fd84f
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-20 19:44:02 -0600

    refactor: standardize response format in HttpCodesController to include status, message, and error fields

--- src/controllers/http-codes.controller.ts
+++ src/controllers/http-codes.controller.ts
@@ -9,11 +9,21 @@ export class HttpCodesController {
     getHttpCodesAll = async (_req: Request, res: Response) => {
         try {
             const dataHttpCodes = await httpCodesModel.getHttpCodes();
-            res.status(200).json(dataHttpCodes);
+            res.status(200).json({
+                ok: true,
+                message: "Datos obtenidos correctamente",
+                data: dataHttpCodes,
+                error: null
+            });
 
         } catch (error) {
             const message = validarMessageError(error, "Error interno del servidor");
-            res.status(500).json({ error: { message: message } });
+            res.status(500).json({
+                ok: false,
+                data: null,
+                message: message,
+                error: error
+            });
         }
     };
 
@@ -26,10 +36,20 @@ export class HttpCodesController {
 
             const httpCodeFound = await httpCodesModel.getHttpCode({ code: codeNumber });
 
-            res.status(200).json(httpCodeFound);
+            res.status(200).json({
+                ok: true,
+                message: "Datos obtenidos correctamente",
+                data: httpCodeFound,
+                error: null
+            });
         } catch (error) {
             const message = validarMessageError(error, "Error interno del servidor");
-            res.status(400).json({ error: { message } });
+            res.status(400).json({
+                ok: false,
+                data: null,
+                message: message,
+                error: error
+            });
         }
     };
 
