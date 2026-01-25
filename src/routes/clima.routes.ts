2183ebc378d7ae006c3356a175f0f4cc140110f9
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-24 23:35:51 -0600

    feat: integrate validation middleware into Clima route for enhanced parameter checking

--- src/routes/clima.routes.ts
+++ src/routes/clima.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { ClimaController } from "../controllers/clima.controller.js";
 import { API_KEY_CLIMA, BASE_URL_CLIMA } from "../constants/index.js";
+import { ClimaController } from "../controllers/clima.controller.js";
+import { validarCampos } from "../middleware/clima.js";
 export const climaRouter = Router();
 
 
 const url = BASE_URL_CLIMA + API_KEY_CLIMA;
 const climaController = new ClimaController(url);
 
-climaRouter.get("/:lugar", climaController.getClima);
+
+
+climaRouter.get("/:lugar", validarCampos, climaController.getClima);
