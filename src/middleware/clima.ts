6b8fc28952e9203506c2c8f7f5885e3fa6b77774
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-24 23:35:46 -0600

    feat: add middleware for validating Clima parameters with structured error responses

---
+++ src/middleware/clima.ts
@@ -0,0 +1,26 @@
+import type { NextFunction, Request, Response } from "express";
+import { validarPartialClimaCampos } from "../utils/Clima/schema.js";
+
+export function validarCampos(req: Request, res: Response, next: NextFunction) {
+    const { days } = req.query as { days: string };
+    const { lugar } = req.params as { lugar: string };
+
+    const result = validarPartialClimaCampos({ lugar, days });
+
+    if (!result.success) {
+        const message = result.error.issues.map(issue => issue.message);
+        res.status(400).json({
+            ok: false,
+            message: "Error en los parámetros",
+            error: {
+                code: result.error.name,
+                message: message.join(", "),
+            },
+            data: null,
+        });
+        return
+    }
+    const { lugar: lugarParam, days: daysParam } = result.data;
+    req.params = { lugar: lugarParam, days: daysParam.toString() };
+    next();
+}
