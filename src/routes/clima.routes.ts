cf9cd064f785d8956d4e25b8b7130dc361f10657
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-25 18:02:18 -0600

    refactor: update Clima route to use new middleware for parameter validation

--- src/routes/clima.routes.ts
+++ src/routes/clima.routes.ts
@@ -1,13 +1,11 @@
 import { Router } from "express";
 import { API_KEY_CLIMA, BASE_URL_CLIMA } from "../constants/index.js";
 import { ClimaController } from "../controllers/clima.controller.js";
-import { validarCampos } from "../middleware/clima.js";
-export const climaRouter = Router();
+import { middlewareClima } from "../middleware/clima.js";
 
+export const climaRouter = Router();
 
 const url = BASE_URL_CLIMA + API_KEY_CLIMA;
 const climaController = new ClimaController(url);
 
-
-
-climaRouter.get("/:lugar", validarCampos, climaController.getClima);
+climaRouter.get("/:lugar", middlewareClima, climaController.getClima);
