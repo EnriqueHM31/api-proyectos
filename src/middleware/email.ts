faca0db719043e16525f30aef1c094cf9320560e
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-25 18:44:23 -0600

    refactor: improve error handling in email middleware by utilizing middlewareError for validation failures

--- src/middleware/email.ts
+++ src/middleware/email.ts
@@ -1,5 +1,6 @@
 import type { NextFunction, Request, Response } from "express";
 import { validarPartialEmailCampos } from "../utils/Email/schema.js";
+import { middlewareError } from "../utils/middleware.js";
 
 export function middlewareEmail(req: Request, res: Response, next: NextFunction) {
     const { email, comentario, page } = req.body;
@@ -7,17 +8,7 @@ export function middlewareEmail(req: Request, res: Response, next: NextFunction)
     const result = validarPartialEmailCampos({ email, comentario, page });
 
     if (!result.success) {
-        const message = result.error.issues[0]?.message ?? "Error en los parámetros";
-        const path = result.error.issues[0]?.path ?? "Datos";
-        res.status(400).json({
-            ok: false,
-            message: "Error en los parámetros",
-            error: {
-                code: `${result.error.name}`,
-                message: `${path}:  ${message}`,
-            },
-            data: null,
-        });
+        middlewareError(result.error, res);
         return
     }
     const { email: emailParam, comentario: comentarioParam, page: pageParam } = result.data;
