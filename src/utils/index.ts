58e2197b1f1822038451ae027f8fb5da2afd8c6d
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-25 19:29:42 -0600

    refactor: add formatoRespuesta function for consistent response formatting and improve export order

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -10,10 +10,21 @@ const __dirname = path.dirname(__filename);
 // 👉 Raíz de src
 const SRC_ROOT = path.resolve(__dirname, "..");
 
-export { __filename, __dirname, SRC_ROOT };
+export { __dirname, __filename, SRC_ROOT };
 
 
 export function validarMessageError(error: any, messageDefecto: string) {
     return error instanceof Error ? error.message : messageDefecto;
 
 }
+
+
+export function formatoRespuesta({ ok, message, error, data }: { ok: boolean, message: string, error: any, data: any }) {
+    return {
+        ok: ok,
+        message: message,
+        error: error,
+        data: data,
+    };
+}
+
