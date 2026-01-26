904cfc6c521f94ac9cb1d777f2fcbdedd73784be
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-25 19:29:34 -0600

    refactor: enhance response formatting in IpController for improved consistency and clarity

--- src/controllers/ip.controller.ts
+++ src/controllers/ip.controller.ts
@@ -2,6 +2,7 @@ import type { Request, Response } from "express";
 import { IpModel } from "../models/local/ip.model.js";
 import { crearUrlGeolocalizacion } from "../utils/Geolocalizacion/index.js";
 import { validarMessageError } from "../utils/index.js";
+import { formatoRespuesta } from "../utils/index.js";
 
 const modelIp = new IpModel();
 
@@ -17,24 +18,14 @@ export class IpController {
 
             const data = await modelIp.getIp({ url: urlGeolocalizacion });
 
-            res.status(200).json({
-                ok: true,
-                message: "Datos obtenidos correctamente",
-                data: data,
-                error: null
-            });
+            const response = formatoRespuesta({ ok: true, message: "Datos obtenidos correctamente", error: null, data });
+            res.status(200).json(response);
 
         } catch (error) {
             const message = validarMessageError(error, "Error interno del servidor");
             if (!res.headersSent) {
-                res.status(500).json(
-                    {
-                        ok: false,
-                        data: null,
-                        message: "Error interno del servidor",
-                        error: message
-                    }
-                );
+                const response = formatoRespuesta({ ok: false, message: "Error interno del servidor", error: message, data: null });
+                res.status(500).json(response);
             }
         }
     };
