318ceb981839651ab03a676def57e2b8c16c720c
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-30 23:09:56 -0600

    feat: implement BibliotecaController to handle library data retrieval

---
+++ src/controllers/biblioteca.controller.ts
@@ -0,0 +1,16 @@
+import type { Request, Response } from "express";
+import { BibliotecaModel } from "../models/local/biblioteca.model.js";
+import { formatoRespuesta } from "../utils/index.js";
+
+
+const bibliotecaModel = new BibliotecaModel();
+
+export class BibliotecaController {
+    async getAll(req: Request, res: Response) {
+
+        const data = await bibliotecaModel.getBiblioteca();
+
+        res.status(200).json(formatoRespuesta({ ok: true, message: "Biblioteca", error: null, data }));
+
+    }
+}
