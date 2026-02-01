6583041234bfd1a07710786ef5bcb6c7143bab84
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-31 20:26:44 -0600

    fix: refine error message formatting in middlewareError function for improved clarity

--- src/utils/middleware.ts
+++ src/utils/middleware.ts
@@ -3,14 +3,13 @@ import type { ZodError } from "zod";
 
 export function middlewareError(result: ZodError, res: Response) {
     const message = result.issues[0]?.message ?? "Error en los parámetros";
-    const path = result.issues[0]?.path ?? "Datos";
 
     res.status(400).json({
         ok: false,
         message: "Error en los parámetros",
         error: {
             code: result.name,
-            message: `${path}:  ${message}`,
+            message: `Error: ${message}`,
         },
         data: null,
     });
