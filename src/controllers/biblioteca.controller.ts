824765b7226f8cc5d904e028a5fe25ab121f8521
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-31 19:01:29 -0600

    feat: implement CRUD operations in BibliotecaController for managing library data

--- src/controllers/biblioteca.controller.ts
+++ src/controllers/biblioteca.controller.ts
@@ -17,4 +17,48 @@ export class BibliotecaController {
         }
 
     }
+
+    async getBook(req: Request, res: Response) {
+        try {
+            const { id } = req.params as { id: string };
+            const { data } = await bibliotecaModel.getBibliotecaById(id);
+
+            res.status(200).json(formatoRespuesta({ ok: true, message: "Biblioteca", error: null, data }));
+        } catch (error) {
+            res.status(500).json(formatoRespuesta({ ok: false, message: "Biblioteca", error: error as string, data: null }));
+        }
+    }
+
+    async create(req: Request, res: Response) {
+        try {
+            const { data } = await bibliotecaModel.createBiblioteca(req.body);
+
+            res.status(200).json(formatoRespuesta({ ok: true, message: "Biblioteca", error: null, data }));
+        } catch (error) {
+            res.status(500).json(formatoRespuesta({ ok: false, message: "Biblioteca", error: error as string, data: null }));
+        }
+    }
+
+    async update(req: Request, res: Response) {
+        try {
+            const { id } = req.params as { id: string };
+            const campos = req.body
+            const { data } = await bibliotecaModel.updateBiblioteca(id, campos);
+
+            res.status(200).json(formatoRespuesta({ ok: true, message: "Biblioteca", error: null, data }));
+        } catch (error) {
+            res.status(500).json(formatoRespuesta({ ok: false, message: "Biblioteca", error: error as string, data: null }));
+        }
+    }
+
+    async delete(req: Request, res: Response) {
+        try {
+            const { id } = req.params as { id: string };
+            const { data } = await bibliotecaModel.deleteBiblioteca(id);
+
+            res.status(200).json(formatoRespuesta({ ok: true, message: "Biblioteca", error: null, data }));
+        } catch (error) {
+            res.status(500).json(formatoRespuesta({ ok: false, message: "Biblioteca", error: error as string, data: null }));
+        }
+    }
 }
