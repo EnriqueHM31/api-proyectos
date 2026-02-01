9c7a14661a316eb3b2dcd793eb3be8e25cfd835e
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-31 19:01:37 -0600

    feat: add CRUD routes for managing library books in bibliotecaRouter

--- src/routes/biblioteca.routes.ts
+++ src/routes/biblioteca.routes.ts
@@ -6,3 +6,11 @@ export const bibliotecaRouter = Router();
 const bibliotecaController = new BibliotecaController();
 
 bibliotecaRouter.get("/", bibliotecaController.getAll);
+
+bibliotecaRouter.get("/:id", bibliotecaController.getBook);
+
+bibliotecaRouter.post("/", bibliotecaController.create);
+
+bibliotecaRouter.put("/:id", bibliotecaController.update);
+
+bibliotecaRouter.delete("/:id", bibliotecaController.delete);
