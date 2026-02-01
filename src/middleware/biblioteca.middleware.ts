28c64c3df664051bd45d4a4c4059497605323723
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-31 20:29:45 -0600

    refactor: streamline biblioteca middleware for improved validation and error handling

--- src/middleware/biblioteca.middleware.ts
+++ src/middleware/biblioteca.middleware.ts
@@ -1,26 +1,32 @@
 import type { NextFunction, Request, Response } from "express";
-import type { ZodError } from "zod";
 import { validarPartialBibliotecaCampos } from "../utils/Biblioteca/schema.js";
 import { middlewareError } from "../utils/middleware.js";
 
 export function middlewareBibliotecaCreate(req: Request, res: Response, next: NextFunction) {
     const data = req.body;
 
-    try {
+    const result = validarPartialBibliotecaCampos(data);
+    console.log(result);
 
-        const result = validarPartialBibliotecaCampos(data);
-        console.log(result);
-
-        if (!result.success) {
-            middlewareError(result.error, res);
-            return
-        }
-        const { id, volumeInfo } = result.data;
-        req.body = { id, volumeInfo };
-        next();
-    } catch (error) {
-        console.log(error);
-        middlewareError(error as ZodError, res);
+    if (!result.success) {
+        middlewareError(result.error, res);
         return
     }
+    const { id, volumeInfo } = result.data;
+    req.body = { id, volumeInfo };
+    next();
+}
+
+export function middlewareBibliotecaId(req: Request, res: Response, next: NextFunction) {
+    const { id } = req.params as { id: string };
+
+    const result = validarPartialBibliotecaCampos({ id });
+
+    if (!result.success) {
+        middlewareError(result.error, res);
+        return
+    }
+    const { id: idParam } = result.data as { id: string };
+    req.params = { id: idParam.toString() };
+    next();
 }
