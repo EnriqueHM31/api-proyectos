41993fad672230144f7db5c5eef81c5415dd099c
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-01 21:34:02 -0600

    feat: add bibliotecaCategories routes for CRUD operations

---
+++ src/routes/biblioteca/bibiliotecaCategories.routes.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+
+import { BibliotecaCategoriesController } from "../../controllers/biblioteca/bibliotecaCategories.controller.js";
+
+import { middlewareBibliotecaCategoriesId, middlewareBibliotecaCategoriesCreate } from "../../middleware/biblioteca/bibliotecaCategories.middleware.js";
+
+export const bibliotecaCategoriesRouter = Router();
+
+const bibliotecaCategoriesController = new BibliotecaCategoriesController();
+
+bibliotecaCategoriesRouter.get("/", bibliotecaCategoriesController.getAll);
+
+bibliotecaCategoriesRouter.get("/:id", middlewareBibliotecaCategoriesId, bibliotecaCategoriesController.getCategories);
+
+bibliotecaCategoriesRouter.post("/", middlewareBibliotecaCategoriesCreate, bibliotecaCategoriesController.create);
+
+bibliotecaCategoriesRouter.put("/:id", middlewareBibliotecaCategoriesId, bibliotecaCategoriesController.update);
+
+bibliotecaCategoriesRouter.delete("/:id", middlewareBibliotecaCategoriesId, bibliotecaCategoriesController.delete); 
