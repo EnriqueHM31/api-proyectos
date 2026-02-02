3d50f7812e3ff17d066b7c48d128774c6b1b8004
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-01 21:33:36 -0600

    feat: add middleware for biblioteca book creation and ID validation

---
+++ src/middleware/biblioteca/bibliotecaBook.middleware.ts
@@ -0,0 +1,32 @@
+import type { NextFunction, Request, Response } from "express";
+import { validarPartialBibliotecaBooksCampos } from "../../utils/Biblioteca/schemaBook.js";
+import { middlewareError } from "../../utils/middleware.js";
+
+export function middlewareBibliotecaBooksCreate(req: Request, res: Response, next: NextFunction) {
+    const data = req.body;
+
+    const result = validarPartialBibliotecaBooksCampos(data);
+    console.log(result);
+
+    if (!result.success) {
+        middlewareError(result.error, res);
+        return
+    }
+    const { id, volumeInfo } = result.data;
+    req.body = { id, volumeInfo };
+    next();
+}
+
+export function middlewareBibliotecaBooksId(req: Request, res: Response, next: NextFunction) {
+    const { id } = req.params as { id: string };
+
+    const result = validarPartialBibliotecaBooksCampos({ id });
+
+    if (!result.success) {
+        middlewareError(result.error, res);
+        return
+    }
+    const { id: idParam } = result.data as { id: string };
+    req.params = { id: idParam.toString() };
+    next();
+}
