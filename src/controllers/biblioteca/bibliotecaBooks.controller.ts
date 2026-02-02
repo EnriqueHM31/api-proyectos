8e5223559f23e244ab84ca1f0f95ef8f38e7f641
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-01 21:33:17 -0600

    feat: implement BibliotecaBooksController with CRUD operations for biblioteca books

---
+++ src/controllers/biblioteca/bibliotecaBooks.controller.ts
@@ -0,0 +1,67 @@
+import type { Request, Response } from "express";
+import { formatoRespuesta } from "../../utils/index.js";
+import { BibliotecaBooksModel } from "../../models/local/biblioteca/bibliotecaBooks.model.js";
+
+
+const bibliotecaModel = new BibliotecaBooksModel();
+
+export class BibliotecaBooksController {
+    async getAll(req: Request, res: Response) {
+
+        try {
+            const { data } = await bibliotecaModel.getBiblioteca();
+
+            res.status(200).json(formatoRespuesta({ ok: true, message: "Biblioteca", error: null, data }));
+        } catch (error) {
+            res.status(500).json(formatoRespuesta({ ok: false, message: "Biblioteca", error: error as string, data: null }));
+        }
+
+    }
+
+    async getBook(req: Request, res: Response) {
+        try {
+            const { id } = req.params as { id: string };
+            const { data } = await bibliotecaModel.getBibliotecaById(id);
+
+            res.status(200).json(formatoRespuesta({ ok: true, message: "Biblioteca", error: null, data }));
+        } catch (error) {
+            res.status(500).json(formatoRespuesta({ ok: false, message: "Biblioteca", error: error as string, data: null }));
+        }
+    }
+
+    async create(req: Request, res: Response) {
+        try {
+            console.log(req.body);
+            const { data } = await bibliotecaModel.createBiblioteca(req.body);
+
+
+            console.log(data);
+            res.status(200).json(formatoRespuesta({ ok: true, message: "Biblioteca", error: null, data }));
+        } catch (error) {
+            res.status(500).json(formatoRespuesta({ ok: false, message: "Biblioteca", error: error as string, data: null }));
+        }
+    }
+
+    async update(req: Request, res: Response) {
+        try {
+            const { id } = req.params as { id: string };
+            const campos = req.body
+            const { data } = await bibliotecaModel.updateBiblioteca(id, campos);
+
+            res.status(200).json(formatoRespuesta({ ok: true, message: "Biblioteca", error: null, data }));
+        } catch (error) {
+            res.status(500).json(formatoRespuesta({ ok: false, message: "Biblioteca", error: error as string, data: null }));
+        }
+    }
+
+    async delete(req: Request, res: Response) {
+        try {
+            const { id } = req.params as { id: string };
+            const { data } = await bibliotecaModel.deleteBiblioteca(id);
+
+            res.status(200).json(formatoRespuesta({ ok: true, message: "Biblioteca", error: null, data }));
+        } catch (error) {
+            res.status(500).json(formatoRespuesta({ ok: false, message: "Biblioteca", error: error as string, data: null }));
+        }
+    }
+}
