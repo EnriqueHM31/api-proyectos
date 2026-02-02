09fca6f801aae8c4378840bec0644c1e9a93eaf4
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-01 21:36:39 -0600

    refactor: improve code formatting and remove unnecessary newlines in bibliotecaCategories.controller.ts

--- src/controllers/biblioteca/bibliotecaCategories.controller.ts
+++ src/controllers/biblioteca/bibliotecaCategories.controller.ts
@@ -1,10 +1,13 @@
+
 import { formatoRespuesta } from "../../utils/index.js";
 import type { Request, Response } from "express";
 import { BibliotecaCategoriesModel } from "../../models/local/biblioteca/bibliotecaCategories.model.js";
 
+
 const bibliotecaCategoriesModel = new BibliotecaCategoriesModel();
 export class BibliotecaCategoriesController {
     async getAll(req: Request, res: Response) {
+
         try {
             const { data } = await bibliotecaCategoriesModel.getBiblioteca();
 
@@ -12,6 +15,7 @@ export class BibliotecaCategoriesController {
         } catch (error) {
             res.status(500).json(formatoRespuesta({ ok: false, message: "Biblioteca", error: error as string, data: null }));
         }
+
     }
 
     async getCategories(req: Request, res: Response) {
@@ -30,6 +34,7 @@ export class BibliotecaCategoriesController {
             console.log(req.body);
             const { data } = await bibliotecaCategoriesModel.createBiblioteca(req.body);
 
+
             console.log(data);
             res.status(200).json(formatoRespuesta({ ok: true, message: "Biblioteca", error: null, data }));
         } catch (error) {
@@ -40,7 +45,7 @@ export class BibliotecaCategoriesController {
     async update(req: Request, res: Response) {
         try {
             const { id } = req.params as { id: string };
-            const campos = req.body;
+            const campos = req.body
             const { data } = await bibliotecaCategoriesModel.updateBiblioteca(id, campos);
 
             res.status(200).json(formatoRespuesta({ ok: true, message: "Biblioteca", error: null, data }));
