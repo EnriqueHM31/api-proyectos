2d52b34e42514c2508218d3dd2b0534cbdd0df4f
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-04 22:07:22 -0600

    feat: add language route to bibliotecaRouter for managing programming languages

--- src/routes/biblioteca.routes.ts
+++ src/routes/biblioteca.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { bibliotecaBooksRouter } from "./biblioteca/bibliotecaBooks.routes.js";
 import { bibliotecaCategoriesRouter } from "./biblioteca/bibiliotecaCategories.routes.js";
+import { bibliotecaLenguajesRouter } from "./biblioteca/bibliotecaLenguajes.routes.js";
 
 export const bibliotecaRouter = Router();
 
 bibliotecaRouter.use("/book", bibliotecaBooksRouter);
 
 bibliotecaRouter.use("/categories", bibliotecaCategoriesRouter);
+
+bibliotecaRouter.use("/language", bibliotecaLenguajesRouter);
