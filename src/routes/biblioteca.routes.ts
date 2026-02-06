7ba78b4f29c95debe1b0d85d216d92e9bf5715be
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-06 17:49:01 -0600

    refactor: update biblioteca router to use renamed bibliotecaLibrosRouter for consistency with naming conventions

--- src/routes/biblioteca.routes.ts
+++ src/routes/biblioteca.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { bibliotecaBooksRouter } from "./biblioteca/bibliotecaLibros.routes.js";
+import { bibliotecaLibrosRouter } from "./biblioteca/bibliotecaLibros.routes.js";
 import { bibliotecaCategoriasRouter } from "./biblioteca/bibiliotecaCategorias.routes.js";
 import { bibliotecaLenguajesRouter } from "./biblioteca/bibliotecaLenguajes.routes.js";
 
 export const bibliotecaRouter = Router();
 
-bibliotecaRouter.use("/libro", bibliotecaBooksRouter);
+bibliotecaRouter.use("/libro", bibliotecaLibrosRouter);
 
 bibliotecaRouter.use("/categorias", bibliotecaCategoriasRouter);
 
