ca0677fa3384d0fe23b6f4735d654df9641e898e
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-06 18:09:17 -0600

    refactor: improve error handling in BibliotecaCategoriasController by extracting error details for better response clarity

--- src/controllers/biblioteca/bibliotecaCategorias.controller.ts
+++ src/controllers/biblioteca/bibliotecaCategorias.controller.ts
@@ -1,16 +1,18 @@
-import { formatoRespuesta } from "../../utils/index.js";
+import { extraerDatosError, formatoRespuesta } from "../../utils/index.js";
 import type { Request, Response } from "express";
 import { BibliotecaCategoriasModel } from "../../models/local/biblioteca/bibliotecaCategorias.model.js";
 
 const bibliotecaCategoriasModel = new BibliotecaCategoriasModel();
+
 export class BibliotecaCategoriasController {
-    async obtenerCategorias(req: Request, res: Response) {
+    async obtenerCategorias(_req: Request, res: Response) {
         try {
             const { data } = await bibliotecaCategoriasModel.obtenerBibliotecaCategorias();
 
             res.status(200).json(formatoRespuesta({ ok: true, message: "Biblioteca Categorias Obtenidas", error: null, data }));
         } catch (error) {
-            res.status(500).json(formatoRespuesta({ ok: false, message: "Biblioteca Categorias Obtenidas", error: error as string, data: null }));
+            const { messageError, errorName } = extraerDatosError(error);
+            res.status(500).json(formatoRespuesta({ ok: false, message: messageError, error: errorName, data: null }));
         }
     }
 
@@ -21,7 +23,8 @@ export class BibliotecaCategoriasController {
 
             res.status(200).json(formatoRespuesta({ ok: true, message: `La categoría ${data?.nombre ?? ""} ha sido encontrada`, error: null, data }));
         } catch (error) {
-            res.status(500).json(formatoRespuesta({ ok: false, message: `Ocurrio un error al obtener la categoría`, error: error as string, data: null }));
+            const { messageError, errorName } = extraerDatosError(error);
+            res.status(500).json(formatoRespuesta({ ok: false, message: messageError, error: errorName, data: null }));
         }
     }
 
@@ -33,7 +36,8 @@ export class BibliotecaCategoriasController {
             console.log(data);
             res.status(200).json(formatoRespuesta({ ok: true, message: `La categoría ${data?.nombre ?? ""} ha sido creada`, error: null, data }));
         } catch (error) {
-            res.status(500).json(formatoRespuesta({ ok: false, message: "Ocurrio un error al crear la categoría", error: error as string, data: null }));
+            const { messageError, errorName } = extraerDatosError(error);
+            res.status(500).json(formatoRespuesta({ ok: false, message: messageError, error: errorName, data: null }));
         }
     }
 
@@ -46,16 +50,10 @@ export class BibliotecaCategoriasController {
 
             const CamposModificados = Object.keys(campos);
             const inicio = CamposModificados.length > 1 ? "Los campos" : "El campo";
-            res.status(200).json(
-                formatoRespuesta({
-                    ok: true,
-                    message: `${inicio}  ${CamposModificados.length > 0 ? CamposModificados.join(", ") : ""} ha sido actualizada`,
-                    error: null,
-                    data,
-                })
-            );
+            res.status(200).json(formatoRespuesta({ ok: true, message: `${inicio}  ${CamposModificados.length > 0 ? CamposModificados.join(", ") : ""} ha sido actualizada`, error: null, data, }));
         } catch (error) {
-            res.status(500).json(formatoRespuesta({ ok: false, message: "Ocurrio un error al actualizar la categoría", error: error as string, data: null }));
+            const { messageError, errorName } = extraerDatosError(error);
+            res.status(500).json(formatoRespuesta({ ok: false, message: messageError, error: errorName, data: null }));
         }
     }
 
@@ -66,7 +64,8 @@ export class BibliotecaCategoriasController {
 
             res.status(200).json(formatoRespuesta({ ok: true, message: `${`La categoría ${data?.nombre ?? ""} ha sido eliminada`}`, error: null, data }));
         } catch (error) {
-            res.status(500).json(formatoRespuesta({ ok: false, message: "Ocurrio un error al eliminar la categoría", error: error as string, data: null }));
+            const { messageError, errorName } = extraerDatosError(error);
+            res.status(500).json(formatoRespuesta({ ok: false, message: messageError, error: errorName, data: null }));
         }
     }
 }
