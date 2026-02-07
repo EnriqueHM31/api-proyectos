035034da277c9c5f21e6fdecde06ef53e0ef942b
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-06 18:11:22 -0600

    refactor: simplify imports and remove redundant comments in bibliotecaLenguajes middleware for better clarity

--- src/middleware/biblioteca/bibliotecaLenguajes.middleware.ts
+++ src/middleware/biblioteca/bibliotecaLenguajes.middleware.ts
@@ -1,23 +1,15 @@
 import type { NextFunction, Request, Response } from "express";
 import type { ZodError } from "zod";
 
-import {
-    validarBibliotecaLenguajesCrear,
-    validarBibliotecaLenguajesModificar,
-    validarBibliotecaLenguajesId,
-} from "../../utils/Biblioteca/schemaLenguajes.js";
+import { validarBibliotecaLenguajesCrear, validarBibliotecaLenguajesModificar, validarBibliotecaLenguajesId } from "../../utils/Biblioteca/schemaLenguajes.js";
 
 import { middlewareError } from "../../utils/middleware.js";
 
-/* =========================
-   Crear (POST)
-   ➜ NO recibe id
-========================= */
+
 export function middlewareBibliotecaLenguajesCrear(req: Request, res: Response, next: NextFunction) {
     try {
         const result = validarBibliotecaLenguajesCrear(req.body);
 
-        // solo nombre y descripcion
         const { nombre, abreviacion } = result;
 
         req.body = { nombre, abreviacion };
@@ -27,10 +19,7 @@ export function middlewareBibliotecaLenguajesCrear(req: Request, res: Response, 
     }
 }
 
-/* =========================
-   Modificar (PUT / PATCH)
-   ➜ todo opcional
-========================= */
+
 export function middlewareBibliotecaLenguajesModificar(req: Request, res: Response, next: NextFunction) {
     const result = validarBibliotecaLenguajesModificar(req.body);
 
@@ -43,10 +32,6 @@ export function middlewareBibliotecaLenguajesModificar(req: Request, res: Respon
     next();
 }
 
-/* =========================
-   ID PARAM (GET / DELETE)
-   ➜ solo UUID
-========================= */
 export function middlewareBibliotecaLenguajesId(req: Request, res: Response, next: NextFunction) {
     const result = validarBibliotecaLenguajesId(req.params);
 
@@ -55,9 +40,7 @@ export function middlewareBibliotecaLenguajesId(req: Request, res: Response, nex
         return;
     }
 
-    req.params = {
-        id: result.data.id,
-    };
+    req.params = { id: result.data.id };
 
     next();
 }
