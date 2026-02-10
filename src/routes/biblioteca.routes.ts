51f138007093f9679b23a7c7611c6b2ee1844563
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-09 21:11:47 -0600

    feat: add authentication routes to bibliotecaRouter for user login and registration

--- src/routes/biblioteca.routes.ts
+++ src/routes/biblioteca.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { bibliotecaLibrosRouter } from "./biblioteca/bibliotecaLibros.routes.js";
 import { bibliotecaCategoriasRouter } from "./biblioteca/bibiliotecaCategorias.routes.js";
 import { bibliotecaLenguajesRouter } from "./biblioteca/bibliotecaLenguajes.routes.js";
+import { bibliotecaAuthRoutes } from "./biblioteca/bibliotecaAuth.routes.js";
 
 export const bibliotecaRouter = Router();
 
@@ -10,3 +11,5 @@ bibliotecaRouter.use("/libro", bibliotecaLibrosRouter);
 bibliotecaRouter.use("/categoria", bibliotecaCategoriasRouter);
 
 bibliotecaRouter.use("/lenguaje", bibliotecaLenguajesRouter);
+
+bibliotecaRouter.use("/autenticacion", bibliotecaAuthRoutes);
