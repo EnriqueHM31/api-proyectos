ab6cfdd5d945e45f8e9d7de27f44ed80a8b00c20
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-10 19:16:57 -0600

    refactor: update import statements and formatting in bibliotecaAuth.routes for consistency and readability

--- src/routes/biblioteca/bibliotecaAuth.routes.ts
+++ src/routes/biblioteca/bibliotecaAuth.routes.ts
@@ -1,8 +1,8 @@
-import { Router } from 'express';
-import { bibliotecaAuthController } from '../../controllers/biblioteca/bibliotecaAuth.controller.js';
+import { Router } from "express";
+import { bibliotecaAuthController } from "../../controllers/biblioteca/bibliotecaAuth.controller.js";
 
 export const bibliotecaAuthRoutes = Router();
 
-bibliotecaAuthRoutes.post('/iniciar-sesion', bibliotecaAuthController.IniciarSesion);
-bibliotecaAuthRoutes.post('/registrar-usuario', bibliotecaAuthController.RegistrarUsuario);
-bibliotecaAuthRoutes.get('/usuario', bibliotecaAuthController.ObtenerUsuario);
+bibliotecaAuthRoutes.post("/iniciar-sesion", bibliotecaAuthController.IniciarSesion);
+bibliotecaAuthRoutes.post("/registrar-usuario", bibliotecaAuthController.RegistrarUsuario);
+bibliotecaAuthRoutes.get("/usuario", bibliotecaAuthController.ObtenerUsuario);
