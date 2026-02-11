e3cc1ff9f638c7f32195faa9eeba4ddc222e430d
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-10 22:38:32 -0600

    feat: add cerrar-sesion route to bibliotecaAuthRoutes for user session termination

--- src/routes/biblioteca/bibliotecaAuth.routes.ts
+++ src/routes/biblioteca/bibliotecaAuth.routes.ts
@@ -6,3 +6,4 @@ export const bibliotecaAuthRoutes = Router();
 bibliotecaAuthRoutes.post("/iniciar-sesion", bibliotecaAuthController.IniciarSesion);
 bibliotecaAuthRoutes.post("/registrar-usuario", bibliotecaAuthController.RegistrarUsuario);
 bibliotecaAuthRoutes.get("/usuario", bibliotecaAuthController.ObtenerUsuario);
+bibliotecaAuthRoutes.post("/cerrar-sesion", bibliotecaAuthController.CerrarSesion);
