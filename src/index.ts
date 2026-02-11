1d103b0bc7aae51bee52d7626cac2b483abec180
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-10 22:38:21 -0600

    feat: integrate cookie-parser middleware and update server listening address to localhost

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
+import cookiesParser from "cookie-parser";
 import "dotenv/config";
 import express from "express";
 import { PORT } from "./config/index.js";
 import { setupStatic } from "./config/static.js";
 import { setupViews } from "./config/view.js";
 import { createCorsMiddleware } from "./middleware/cors.middleware.js";
+import { bibliotecaRouter } from "./routes/biblioteca.routes.js";
 import { climaRouter } from "./routes/clima.routes.js";
+import emailRouter from "./routes/email.routes.js";
 import { geoIpRouter } from "./routes/geoIp.routes.js";
 import { homeRouter } from "./routes/home.routes.js";
 import { httpCodesRouter } from "./routes/http-codes.routes.js";
-import emailRouter from "./routes/email.routes.js";
-import { bibliotecaRouter } from "./routes/biblioteca.routes.js";
 
 /*------------------ APP ------------------*/
 const app = express();
 app.use(createCorsMiddleware());
 app.use(express.json());
+
+app.use(cookiesParser());
 
 /*------------------ VIEWS ------------------*/
 setupViews(app);
@@ -29,6 +32,6 @@ app.use("/biblioteca", bibliotecaRouter);
 app.use("/email", emailRouter);
 
 /*------------------ LISTEN ------------------*/
-app.listen(PORT, "192.168.1.73", () => {
-    console.log(`Server running on http://192.168.1.73:${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
