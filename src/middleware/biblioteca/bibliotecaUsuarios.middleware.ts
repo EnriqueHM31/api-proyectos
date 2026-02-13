459ed86c0d6ed2f95a94ab702fbed62b66176a4e
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-12 21:01:48 -0600

    feat: implement middleware for Biblioteca user creation, modification, and ID validation

---
+++ src/middleware/biblioteca/bibliotecaUsuarios.middleware.ts
@@ -0,0 +1,43 @@
+import type { NextFunction, Request, Response } from "express";
+import type { ZodError } from "zod";
+
+import { validarBibliotecaUsuarioCrear, validarBibliotecaUsuarioModificar, validarBibliotecaUsuarioId } from "../../utils/Biblioteca/schemaUsuario.js";
+import { middlewareError } from "../../utils/middleware.js";
+
+export function middlewareBibliotecaUsuarioCrear(req: Request, res: Response, next: NextFunction) {
+    try {
+        const result = validarBibliotecaUsuarioCrear(req.body);
+
+        const { username, password, correo } = result;
+
+        req.body = { username, password, correo };
+        next();
+    } catch (error) {
+        middlewareError(error as ZodError, res);
+    }
+}
+
+export function middlewareBibliotecaUsuarioModificar(req: Request, res: Response, next: NextFunction) {
+    const result = validarBibliotecaUsuarioModificar(req.body);
+
+    if (!result.success) {
+        middlewareError(result.error, res);
+        return;
+    }
+
+    req.body = result.data;
+    next();
+}
+
+export function middlewareBibliotecaUsuarioId(req: Request, res: Response, next: NextFunction) {
+    const result = validarBibliotecaUsuarioId(req.params);
+
+    if (!result.success) {
+        middlewareError(result.error, res);
+        return;
+    }
+
+    req.params = { id: result.data.id };
+
+    next();
+}
