71d87de1e7e20c6804a21d0b8766905899c10ce4
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-12 21:00:03 -0600

    feat: add user schema validation for Biblioteca with create, modify, and ID validation functions

---
+++ src/utils/Biblioteca/schemaUsuario.ts
@@ -0,0 +1,37 @@
+import { z } from "zod";
+
+export const schemaUsuario = z.object({
+    id: z.string({ message: "El id es requerido" }).uuid({ message: "El id debe ser un UUID válido" }),
+    username: z.string({ message: "El nombre de usuario es requerido" }).min(3, { message: "El nombre de usuario debe tener al menos 3 caracteres" }).max(20, { message: "El nombre de usuario debe tener al menos 3 caracteres" }),
+    password: z.string({ message: "La contraseña es requerida" }).min(8, { message: "La contraseña debe tener al menos 8 caracteres" }).max(20, { message: "La contraseña debe tener al menos 8 caracteres" }),
+    correo: z.string({ message: "El correo es requerido" }).email({ message: "El correo debe ser un correo válido" }),
+});
+
+
+const bibliotecaUsuarioCreareSchema = schemaUsuario.omit({
+    id: true,
+});
+
+export function validarBibliotecaUsuarioCrear(data: unknown) {
+    return bibliotecaUsuarioCreareSchema.parse(data);
+}
+
+const bibliotecaUsuarioModificarSchema = schemaUsuario
+    .omit({ id: true })
+    .partial()
+    .refine((data) => Object.keys(data).length > 0, {
+        message: "Debe enviar al menos un campo para actualizar",
+    });
+
+
+export function validarBibliotecaUsuarioModificar(data: unknown) {
+    return bibliotecaUsuarioModificarSchema.safeParse(data);
+}
+
+const bibliotecaUsuarioIdSchema = z.object({
+    id: z.string({ message: "El id es requerido" }).uuid({ message: "El id debe ser un UUID válido" }),
+});
+
+export function validarBibliotecaUsuarioId(data: unknown) {
+    return bibliotecaUsuarioIdSchema.safeParse(data);
+}
