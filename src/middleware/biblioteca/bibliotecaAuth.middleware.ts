422edde3662606b1670a4c6f5594a3902ae5e33a
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-12 21:07:39 -0600

    feat: add authentication middleware for Biblioteca to enforce user session validation

---
+++ src/middleware/biblioteca/bibliotecaAuth.middleware.ts
@@ -0,0 +1,23 @@
+import type { Request, Response, NextFunction } from "express";
+import { formatoRespuesta } from "../../utils/index.js";
+
+export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.cookies?.token;
+
+    if (!token) {
+        return res.status(401).json(
+            formatoRespuesta({
+                ok: false,
+                message: "No se ha iniciado sesión",
+                data: null,
+                error: null,
+            })
+        );
+        return
+    }
+
+    // opcional: adjuntar token al request para uso posterior
+    req.cookies.token = token as string;
+
+    next();
+};
