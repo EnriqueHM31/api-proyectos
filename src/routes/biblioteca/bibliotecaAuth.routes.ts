0ccdf629b66872bca78d12925f2f85a177eb22fe
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-12 21:23:39 -0600

    fix: update login middleware for Biblioteca authentication session initiation

--- src/routes/biblioteca/bibliotecaAuth.routes.ts
+++ src/routes/biblioteca/bibliotecaAuth.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { bibliotecaAuthController } from "../../controllers/biblioteca/bibliotecaAuth.controller.js";
-import { middlewareBibliotecaUsuarioCrear, middlewareBibliotecaUsuarioModificar } from "../../middleware/biblioteca/bibliotecaUsuarios.middleware.js";
+import { middlewareBibliotecaUsuarioCrear, middlewareBibliotecaUsuarioLogin } from "../../middleware/biblioteca/bibliotecaUsuarios.middleware.js";
 import { middlewareAuth } from "../../middleware/biblioteca/bibliotecaAuth.middleware.js";
 export const bibliotecaAuthRoutes = Router();
 
-bibliotecaAuthRoutes.post("/iniciar-sesion", middlewareBibliotecaUsuarioModificar, bibliotecaAuthController.IniciarSesion);
+bibliotecaAuthRoutes.post("/iniciar-sesion", middlewareBibliotecaUsuarioLogin, bibliotecaAuthController.IniciarSesion);
 bibliotecaAuthRoutes.post("/registrar-usuario", middlewareBibliotecaUsuarioCrear, bibliotecaAuthController.RegistrarUsuario);
 bibliotecaAuthRoutes.get("/usuario", bibliotecaAuthController.ObtenerUsuario);
 bibliotecaAuthRoutes.post("/cerrar-sesion", middlewareAuth, bibliotecaAuthController.CerrarSesion);
