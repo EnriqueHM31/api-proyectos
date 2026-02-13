ce81a772253106c50480df3dc75c7f104da33dea
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-12 21:03:37 -0600

    feat: add middleware for user registration in Biblioteca authentication routes

--- src/routes/biblioteca/bibliotecaAuth.routes.ts
+++ src/routes/biblioteca/bibliotecaAuth.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { bibliotecaAuthController } from "../../controllers/biblioteca/bibliotecaAuth.controller.js";
-import { middlewareBibliotecaUsuarioCrear, middlewareBibliotecaUsuarioModificar, middlewareBibliotecaUsuarioId } from "../../middleware/biblioteca/bibliotecaUsuarios.middleware.js";
+import { middlewareBibliotecaUsuarioCrear, middlewareBibliotecaUsuarioId } from "../../middleware/biblioteca/bibliotecaUsuarios.middleware.js";
 export const bibliotecaAuthRoutes = Router();
 
 bibliotecaAuthRoutes.post("/iniciar-sesion", bibliotecaAuthController.IniciarSesion);
-bibliotecaAuthRoutes.post("/registrar-usuario", bibliotecaAuthController.RegistrarUsuario);
+bibliotecaAuthRoutes.post("/registrar-usuario", middlewareBibliotecaUsuarioCrear, bibliotecaAuthController.RegistrarUsuario);
 bibliotecaAuthRoutes.get("/usuario", bibliotecaAuthController.ObtenerUsuario);
 bibliotecaAuthRoutes.post("/cerrar-sesion", bibliotecaAuthController.CerrarSesion);
