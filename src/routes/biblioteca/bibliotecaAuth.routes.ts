be5d7d67da066829083a8deebaf5ca6b7cde2a2f
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-12 21:17:37 -0600

    feat: add authentication middleware to session termination route in Biblioteca

--- src/routes/biblioteca/bibliotecaAuth.routes.ts
+++ src/routes/biblioteca/bibliotecaAuth.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { bibliotecaAuthController } from "../../controllers/biblioteca/bibliotecaAuth.controller.js";
 import { middlewareBibliotecaUsuarioCrear } from "../../middleware/biblioteca/bibliotecaUsuarios.middleware.js";
+import { middlewareAuth } from "../../middleware/biblioteca/bibliotecaAuth.middleware.js";
 export const bibliotecaAuthRoutes = Router();
 
 bibliotecaAuthRoutes.post("/iniciar-sesion", bibliotecaAuthController.IniciarSesion);
 bibliotecaAuthRoutes.post("/registrar-usuario", middlewareBibliotecaUsuarioCrear, bibliotecaAuthController.RegistrarUsuario);
 bibliotecaAuthRoutes.get("/usuario", bibliotecaAuthController.ObtenerUsuario);
-bibliotecaAuthRoutes.post("/cerrar-sesion", bibliotecaAuthController.CerrarSesion);
+bibliotecaAuthRoutes.post("/cerrar-sesion", middlewareAuth, bibliotecaAuthController.CerrarSesion);
